15c36e71a3b31616b893fc01de8536fdaf079be3
Theodorus Clarence <theodorusclarence@gmail.com>
2023-01-21 10:48:51 +0700

    feat: add no verify to push

--- git-emergency.sh
+++ git-emergency.sh
@@ -9,8 +9,9 @@ timestamp=$(date +"%d-%m-%Y-%H-%M-%S")
 git checkout -b "$name-$timestamp-emergency"
 git add .
 git commit -nm "feat: emergency commit"
-git push origin "$name-$timestamp-emergency"
+git push --no-verify origin "$name-$timestamp-emergency"
 
+echo ""
 echo "-------------"
 echo "Emergency commit created on branch $name-$timestamp-emergency"
 echo "You may now save yourself"
